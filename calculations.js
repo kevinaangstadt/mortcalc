02322907f461343757b283ab687e2981651f71cf
Kevin Angstadt <angstadt@virginia.edu>
2019-12-29 17:37:25 -0500

    Bug fixes and adding new feature

--- calculations.js
+++ calculations.js
@@ -1,10 +1,19 @@
 $(function(){
+  const format = (num, decimals) => num.toLocaleString('en-US', {
+    minimumFractionDigits: 2,      
+    maximumFractionDigits: 2,
+  });
+  
   function numberWithCommas(x) {
-    return x.toString().replace(/\,/g,'').replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    // 20 is the max
+    return parseFloat(x.toString().replace(/\,/g,'')).toLocaleString('en-US', {maximumFractionDigits: 20});
+    //return x.toString().replace(/\,/g,'').replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
   
   $("input").on("keyup", function(evt){
-    $(evt.target).val(numberWithCommas($(evt.target).val()));
+    if($(evt.target).val()) {
+      $(evt.target).val(numberWithCommas($(evt.target).val()));
+    }
   });
   
   $("#principalForm").submit(function(evt) {
@@ -20,7 +29,7 @@ $(function(){
     
     let X = 1.0 + J;
     
-    $("#outputPrincipal").val( numberWithCommas((M / (J / (1 - Math.pow(X, -N)))).toFixed(2)));
+    $("#outputPrincipal").val( numberWithCommas(format((M / (J / (1 - Math.pow(X, -N)))))));
   });
   
   $("#monthlyForm").submit(function(evt) {
@@ -36,6 +45,23 @@ $(function(){
     
     let X = 1.0 + J;
     
-    $("#outputMonthly").val( numberWithCommas(P * ( J / (1 - Math.pow(X, -N))).toFixed(2)));
+    $("#outputMonthly").val( numberWithCommas(format(P * ( J / (1 - Math.pow(X, -N))))));
+  });
+  
+  $("#lengthForm").submit(function(evt) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    
+    let P = parseFloat($("#l-principal").val().replace(/\,/g,''));
+    let M = parseFloat($("#l-monthly").val().replace(/\,/g,''));
+    let I = parseFloat($("#l-rate").val().replace(/\,/g,''));
+    
+    let J = I / (12*100);
+    
+    let X = 1.0 + J;
+    
+    $("#outputLength").val( numberWithCommas(
+      Math.abs(Math.log(1 - (P*J/M)) / (12 * Math.log(X)))
+    ));
   });
 });
