7115cc6f36adfd27ff4c185bb60144faf3db4eef
Kevin Angstadt <kevinaangstadt@users.noreply.github.com>
2022-04-30 12:21:55 -0400

    fix preload

--- preload.js
+++ preload.js
@@ -1,16 +1,6 @@
-const path = require("path");
-
 window.onload = () => {
-    const libraries = [
-        path.join(__dirname, "node_modules/jquery/dist/jquery.min.js"),
-        path.join(__dirname, "node_modules/popper.js/dist/umd/popper.min.js"),
-        path.join(__dirname, "node_modules/bootstrap/dist/js/bootstrap.min.js"),
-        "./calculations.js"
-    ];
-
-    libraries.forEach((p) => {
-        const script = document.createElement("script");
-        script.src = p;
-        document.body.appendChild(script);
-    });    
+    window.$ = window.jQuery = require('jquery');
+    require("popper.js");
+    require("bootstrap");
+    require("./calculations.js");
 }
